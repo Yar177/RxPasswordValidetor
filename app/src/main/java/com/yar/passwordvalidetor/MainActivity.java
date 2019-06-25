27dcfbcecd5657c62f3b4c2ee255f66ff1166d8e
Yar177 <yarsher.dev@gmail.com>
2019-06-25 15:06:51 -0400

    UI

--- app/src/main/java/com/yar/passwordvalidetor/MainActivity.java
+++ app/src/main/java/com/yar/passwordvalidetor/MainActivity.java
@@ -2,8 +2,10 @@ package com.yar.passwordvalidetor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
 
         submit = (Button) findViewById(R.id.password_update_submit_btn);
         cancel = (Button) findViewById(R.id.password_update_cancel_btn);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Password Validated", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Canceled", Toast.LENGTH_LONG).show();
+
+            }
+        });
 
 
 
