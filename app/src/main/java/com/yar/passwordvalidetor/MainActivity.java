d6cf6836398737fd2c2d490dcda328c1887503f8
Yar177 <yarsher.dev@gmail.com>
2019-06-25 14:57:22 -0400

    init

--- app/src/main/java/com/yar/passwordvalidetor/MainActivity.java
+++ app/src/main/java/com/yar/passwordvalidetor/MainActivity.java
@@ -2,12 +2,31 @@ package com.yar.passwordvalidetor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText createPassword;
+    EditText confirmPassword;
+
+    Button submit;
+    Button cancel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        createPassword = (EditText) findViewById(R.id.update_password_create_input_txt);
+        confirmPassword = (EditText) findViewById(R.id.update_password_confirm_input_txt);
+
+        submit = (Button) findViewById(R.id.password_update_submit_btn);
+        cancel = (Button) findViewById(R.id.password_update_cancel_btn);
+
+
+
     }
 }
